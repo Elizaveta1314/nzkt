d89d9002674f21d458437de51f3619665fefab2e
liza <liza.haritonova@mail.ru>
2023-02-10 03:34:09 +0700

    pravki

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
+const modalBtns = document.querySelectorAll('.modal-open');
+const modals = document.querySelectorAll('.modal');
+
+const body = document.body;
+
+function openModal(elem) {
+	elem.classList.add('active');
+	body.classList.add('_locked')
+}
+
+function closeModal(e) {
+	if (e.target.classList.contains('modal-close') || e.target.closest('.modal-close') || e.target.classList.contains('modal-bg')) {
+		e.target.closest('.modal').classList.remove('active');
+		body.classList.remove('_locked')
+	}
+}
+
+modalBtns.forEach(btn => {
+	btn.addEventListener('click', (e) => {
+		let data = e.target.dataset.modalOpen;
+
+		modals.forEach(modal => {
+			if (modal.dataset.modal == data || modal.dataset.modal == e.target.closest('.modal-open').dataset.modalOpen) {
+				openModal(modal)
+			}
+		})
+	})
+})
+
+modals.forEach(modal => {
+	modal.addEventListener('click', e => closeModal(e))
+})
+
+window.addEventListener('keydown', e => {
+	modals.forEach(modal => {
+		if (e.key === "Escape" && modal.classList.contains('active')) {
+			modal.classList.remove('active');
+			body.classList.remove('_locked');
+		}
+	})
+})
+
 // menu
 let menuBtn = document.querySelector('.menu__mobile');
 let menu = document.querySelector('.list_mobile');
 const closeElem = document.querySelector('.menu__close');
 
 closeElem.addEventListener('click', () => {
-  menu.classList.remove('active');
+  menu.classList.remove('active_m');
 });
 menuBtn.addEventListener('click', function(){
-	menu.classList.toggle('active');
+	menu.classList.toggle('active_m');
 });
 
 // табы
@@ -19,10 +61,10 @@ let btns = document.getElementsByClassName("tabs__item");
 for (let item of headerItem) {
     item.addEventListener('click', function() {
         for (let element of mainContent) {
-            element.classList.add('active');
+            element.classList.add('active-t');
         }
         const content = document.querySelector('#' + item.dataset.tab);
-        content.classList.remove('active');
+        content.classList.remove('active-t');
     });
 }
 // Изменение цвета кнопки
